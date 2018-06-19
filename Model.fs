82ded6895cf41ea1cc76c821c034abf4a0321f0b
Christopher <chrispritchard.nz@gmail.com>
2018-06-19 16:51:43 +1200

    Added a world type, and a process command function

--- Model.fs
+++ Model.fs
@@ -3,6 +3,7 @@ module Model
 let width,height = 10,20
 
 type ShapeBlock = | X | O
+
 let shapes = [
     [
         [X;X]
@@ -38,4 +39,28 @@ let plot (tlx, tly) =
     List.mapi (fun y -> 
         List.mapi (fun x -> function
         | X -> (x + tlx, y + tly) |> Some
-        | O -> None) >> List.choose id)
+        | O -> None) >> List.choose id) >> List.concat
+
+type World = {
+    timeBetweenDrops: float
+    staticBlocks: (int * int) list
+    pos: int * int
+    shape: ShapeBlock list list
+}
+
+type Commands = | None | Left | Right | Rotate | Drop
+
+let processCommand world command =
+    let (x, y) = world.pos
+    let (nx, ny) = 
+        match command with
+        | None | Rotate -> (x, y)
+        | Left -> (x - 1, y)
+        | Right -> (x + 1, y)
+        | Drop -> (x, y + 1)
+        
+    let newShape = if command = Rotate then rotate world.shape else world.shape
+    let newBlocks = plot (nx, ny) newShape
+
+    if List.except world.staticBlocks newBlocks <> newBlocks then world
+    else { world with shape = newShape; pos = (nx, ny) }
