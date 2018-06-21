e612359b6230dd16a1c14ae2e2a686245033a121
Chris Pritchard <chris.pritchard@edpay.nz>
2018-06-22 11:54:55 +1200

    Initial view work - rendering the game space

--- View.fs
+++ View.fs
@@ -1,8 +1,9 @@
 module View
 
 open GameCore
+open Microsoft.Xna.Framework
 
-let resolution = Windowed (300,600)
+let resolution = Windowed (400,600)
 
 let assetsToLoad = [
     Texture { key = "blank"; path = "Content/white" }
@@ -10,5 +11,11 @@ let assetsToLoad = [
     Font { key = "default"; path = "Content/coders_crux" }
 ]
 
+// Game space
+let gx, gy, gw, gh = 10,10,200,400
+
 let getView runState model = 
-    []
+    [
+        ColouredImage (Color.Black, { assetKey = "blank"; destRect = gx-1, gy-1, gw+2, gh+2; sourceRect = None })
+        ColouredImage (Color.Gray, { assetKey = "blank"; destRect = gx, gy, gw, gh; sourceRect = None })
+    ]
